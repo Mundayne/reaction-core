ab98dd3c3e9298b82e401a8a36ed122df1ed7f1c
Mundayne <mundanebackflip@gmail.com>
2018-05-10 19:07:40 +1200

    `sendMenu()` should return Promise

--- src/extensions/TextChannel.js
+++ src/extensions/TextChannel.js
@@ -2,19 +2,22 @@ const Discord = require('discord.js')
 const Menu = require('../menu')
 
 Discord.TextChannel.prototype.sendMenu = async function (menu) {
-  if (menu instanceof Menu) {
-    let sendMessage = { }
-    if (typeof menu.text === 'string') {
-      sendMessage = menu.text
-    } else {
-      sendMessage = { embed: menu.text }
-    }
-    this.send(sendMessage).then(async message => {
-      for (let button of menu.buttons) {
-        await message.react(button).catch(console.error)
+  return new Promise((resolve, reject) => {
+    if (menu instanceof Menu) {
+      let sendMessage = { }
+      if (typeof menu.text === 'string') {
+        sendMessage = menu.text
+      } else {
+        sendMessage = { embed: menu.text }
       }
-      menu.register(message)
-      return message
-    })
-  } else return this.send(menu)
+      this.send(sendMessage).then(async message => {
+        console.log('ext msg : ' + message.channel.id)
+        for (let button in menu.buttons) {
+          await message.react(button).catch(console.error)
+        }
+        menu.register(message)
+        resolve(message)
+      })
+    } else this.send(menu).then(message => resolve(message))
+  })
 }
